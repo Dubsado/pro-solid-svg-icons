45fbbb906504c1c237811447c2a96199778d8ccf
Daniel Di Venere <danieldivenere@gmail.com>
2024-01-08 14:51:10 -0800

    init

---
+++ faCampfire.js
@@ -0,0 +1,30 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'fas';
+var iconName = 'campfire';
+var width = 512;
+var height = 512;
+var aliases = [];
+var unicode = 'f6ba';
+var svgPathData = 'M257.5 320c80.3 0 145.5-65.1 145.5-145.5c0-29-14.1-76.4-67.6-129.5c-10.7-10.6-27.8-9.7-38.5 .9L286.5 56.3 244.7 14.5C239.2 9 232 6 224.7 6.1c-5.4 .1-10.7 1.8-15.2 5.3C168.8 43 112 104.9 112 174.5C112 254.9 177.1 320 257.5 320zm58.2-101.7c0 32.1-26 58.2-58.2 58.2s-58.2-26-58.2-58.2c0-33.2 33.7-66.4 49.8-80.4c4.9-4.2 11.9-4.2 16.8 0c16.1 14 49.8 47.2 49.8 80.4zM42.8 289.9c-16.6-5.9-35 2.7-40.9 19.4s2.7 35 19.4 40.9L160.9 400 21.2 449.9C4.6 455.8-4.1 474.1 1.9 490.8s24.3 25.3 40.9 19.4L256 434l213.2 76.2c16.6 5.9 35-2.7 40.9-19.4s-2.7-35-19.4-40.9L351.1 400l139.6-49.9c16.6-5.9 25.3-24.3 19.4-40.9s-24.3-25.3-40.9-19.4L256 366 42.8 289.9z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faCampfire = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
